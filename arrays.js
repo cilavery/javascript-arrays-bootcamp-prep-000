9e11de959583707e20530cb771f57fc50cc922a7
Cecilia Avery <cilsong@gmail.com>
2017-12-28 02:31:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,16 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, el) {
+  var newArr = [el,...arr]
+  return newArr
+}
+
+
+function destructivelyAddElementToBeginningOfArray(arr, el) {
+  arr.push(el)
+  return arr
+}
+
+function accessElementInArray(arr,idx) {
+  return arr[idx]
+}
